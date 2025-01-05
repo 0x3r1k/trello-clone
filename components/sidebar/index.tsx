a4a4bda483913be5430a2a0eeb4055e678ceab76
3r1k <gatodevgames@gmail.com>
2025-01-05 13:28:51 +0100

    Workspaces SQL Fetch

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 
 import { NavMain } from "@/components/sidebar/nav-main";
--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   SidebarMenu,
   SidebarMenuButton,
