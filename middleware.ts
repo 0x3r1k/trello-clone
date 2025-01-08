b31acf4fe6516fa10165361307aaf8fb6f3408a2
3r1k <gatodevgames@gmail.com>
2025-01-08 12:52:36 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { betterFetch } from "@better-fetch/fetch";
 import { NextResponse, type NextRequest } from "next/server";
-import { neon } from "@neondatabase/serverless";
 import type { Session } from "@/lib/auth";
+
+import { getUserFromWorkspace } from "@/actions/user";
+import { getWorkspace } from "@/actions/workspace";
 
 const authRoutes = ["/auth/login", "/auth/register"];
 const passwordRoutes = ["/auth/change-password", "/auth/reset-password"];
@@ -18,10 +20,10 @@ export default async function authMiddleware(request: NextRequest) {
       headers: {
         cookie: request.headers.get("cookie") || "",
       },
-    },
+    }
   );
 
-  if (!session) {
+  if (!session || !session.user) {
     if (isAuthRoute || isPasswordRoute) {
       return NextResponse.next();
     }
@@ -39,20 +41,17 @@ export default async function authMiddleware(request: NextRequest) {
   // Si esta en la ruta, comprobar si el workspace es publico y en caso contrario comprobar si el usuario esta como miembro en el workspace
   // Si no es miembro, redirigir a la pagina de inicio
   if (pathName.startsWith("/workspace/")) {
-    const sql = neon(process.env.DATABASE_URL as string);
     const workspaceId = pathName.split("/")[2];
-    const result =
-      await sql`SELECT * FROM workspace WHERE id = ${workspaceId} LIMIT 1`;
+    const workspace = await getWorkspace(workspaceId);
 
-    if (!result[0]) {
+    if (!workspace) {
       return NextResponse.redirect(new URL("/boards", request.nextUrl));
     }
 
-    if (result[0].visibility === "private") {
-      const user =
-        await sql`SELECT * FROM workspace_members WHERE workspace_id = ${workspaceId} AND user_id = ${session.user.id} LIMIT 1`;
+    if (workspace.visibility === "private") {
+      const user = await getUserFromWorkspace(workspaceId, session.user.id);
 
-      if (!user[0]) {
+      if (!user) {
         return NextResponse.redirect(new URL("/", request.nextUrl));
       }
     }
