b613cd13066be6d69f1e6a88e11404155b23eae4
3r1k <gatodevgames@gmail.com>
2025-01-08 12:58:05 +0100

    fix edge error
    no entiendo la vd

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 import { betterFetch } from "@better-fetch/fetch";
 import { NextResponse, type NextRequest } from "next/server";
+import { neon } from "@neondatabase/serverless";
 import type { Session } from "@/lib/auth";
-
-import { getUserFromWorkspace } from "@/actions/user";
-import { getWorkspace } from "@/actions/workspace";
 
 const authRoutes = ["/auth/login", "/auth/register"];
 const passwordRoutes = ["/auth/change-password", "/auth/reset-password"];
@@ -23,7 +21,7 @@ export default async function authMiddleware(request: NextRequest) {
     }
   );
 
-  if (!session || !session.user) {
+  if (!session) {
     if (isAuthRoute || isPasswordRoute) {
       return NextResponse.next();
     }
@@ -41,17 +39,20 @@ export default async function authMiddleware(request: NextRequest) {
   // Si esta en la ruta, comprobar si el workspace es publico y en caso contrario comprobar si el usuario esta como miembro en el workspace
   // Si no es miembro, redirigir a la pagina de inicio
   if (pathName.startsWith("/workspace/")) {
+    const sql = neon(process.env.DATABASE_URL as string);
     const workspaceId = pathName.split("/")[2];
-    const workspace = await getWorkspace(workspaceId);
+    const result =
+      await sql`SELECT * FROM workspace WHERE id = ${workspaceId} LIMIT 1`;
 
-    if (!workspace) {
+    if (!result[0]) {
       return NextResponse.redirect(new URL("/boards", request.nextUrl));
     }
 
-    if (workspace.visibility === "private") {
-      const user = await getUserFromWorkspace(workspaceId, session.user.id);
+    if (result[0].visibility === "private") {
+      const user =
+        await sql`SELECT * FROM workspace_members WHERE workspace_id = ${workspaceId} AND user_id = ${session.user.id} LIMIT 1`;
 
-      if (!user) {
+      if (!user[0]) {
         return NextResponse.redirect(new URL("/", request.nextUrl));
       }
     }
